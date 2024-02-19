15efbc4869ea97d58d234e9d76509eef85412988
HaByeong <121917642+HaByeong@users.noreply.github.com>
2024-02-19 14:49:08 +0900

    Hello 서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    // /hello 로 보낸 요청 객체를 service 메서드에서 파라미터로 받는다
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello " + username);
+    }
+
+}
